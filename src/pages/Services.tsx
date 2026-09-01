ef122b8d4184ba90b398a92046b55766eb3fb25c
Kaleb Mulugeta Estifanos <kalebmulugeta07@gmail.com>
2026-09-01 13:26:29 -0500

    Update Services.tsx

--- src/pages/Services.tsx
+++ src/pages/Services.tsx
@@ -9,8 +9,7 @@ import Navbar from '../components/layout/Navbar';
 import Footer from '../components/layout/Footer';
 import { services } from '../data/servicesData';
 
-// ============================================
-// TYPES & DATA
+// TYPES AND DATA
 // ============================================
 
 interface Service {
